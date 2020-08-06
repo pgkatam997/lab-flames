e5b3fd504a3683ade45684ffe66ded20cf16c425
Purnima G Katam (S-59) <68459602+pgkatam997@users.noreply.github.com>
2020-08-06 15:14:50 +0530

    prograd id

--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -1,5 +1,37 @@
 package service;
-
+public class FlamesCheckService{
+	public char findFlames(String name1,String name2) {
+		int total=name1.length()+name2.length();
+		int cancel=0;
+		for(int i=0;i<name1.length();i++) {
+			for(int j=0;j<name2.length();j++) {
+				if(name1.charAt(i)==name2.charAt(j)) {
+					++cancel;
+					name2=name2.substring(0,j)+name2.substring(j+1,name2.length());
+					break;
+				}
+			}
+		}
+		int cutting=total-(cancel*2);
+		String flames="flames";
+	StringBuilder check =new StringBuilder(flames);
+	char flameResult=0;
+	while(check.length()!=1) {
+		int x=cutting%check.length();
+		String temp;
+		if(x!=0) {
+			temp=check.substring(x)+check.substring(0,x-1);
+			
+		}
+		else {
+			temp=check.substring(0,check.length()-1);
+		}
+		check= new StringBuilder(temp);	
+	}
+	flameResult=check.charAt(0);
+	return flameResult;
+	}
+}
 // Create a class called FlamesCheckService
 // FlamesCheckService has a method findFlames to find the flames between two names
 // char findFlames(String name1, String name2) takes two strings as arguments
